171a02becb43fddb05a9bb0e631932e552625e87
Jordan <jtate1166@gmail.com>
2022-04-18 13:49:39 -0400

    Create landing page

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Start from "../Start/Start";
+
+export default function App() {
+  return (
+    <div>
+      <Start />
+    </div>
+  );
+}
---
+++ src/Start/Start.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Start.css";
+
+export default function Start() {
+  return (
+    <main>
+      <h1>Quizzical</h1>
+      <p>5 random trivial questions. Can you answer them all correctly?</p>
+      <button>Start Quiz</button>
+    </main>
+  );
+}
