bd0de713f016180fa9f01c01652f903657e03f5b
Jordan <jtate1166@gmail.com>
2022-04-18 17:36:47 -0400

    Dynamically render Quiz component when Start Quiz button is pressed

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,15 @@ import Quiz from "../Quiz/Quiz";
 import { useState } from "react";
 
 export default function App() {
-  const [startQuiz, setStartQuiz] = useState(true);
-  return <div>{startQuiz ? <Quiz /> : <Start />}</div>;
+  const [startQuiz, setStartQuiz] = useState(false);
+  // function passed as prop to the Start component
+  // When Start Quiz button is pressed, startQuiz state is changed from false to true
+  function handleClick() {
+    setStartQuiz(true);
+  }
+  // if startQuiz state equals 'true', render Quiz component
+  // if startQuiz state equals 'false', render Start component
+  return (
+    <div>{startQuiz ? <Quiz /> : <Start handleClick={handleClick} />}</div>
+  );
 }
--- src/Start/Start.js
+++ src/Start/Start.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Start.css";
 
-export default function Start() {
+export default function Start(props) {
   return (
     <main>
       <h1>Quizzical</h1>
       <p>5 random trivial questions. Can you answer them all correctly?</p>
-      <button>Start Quiz</button>
+      <button onClick={props.handleClick}>Start Quiz</button>
     </main>
   );
 }
