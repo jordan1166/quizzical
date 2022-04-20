c170cf0d7bfee5396968b24d2fb130f0ed2fb841
Jordan <jtate1166@gmail.com>
2022-04-20 12:52:13 -0400

    Show score when check answer button is clicked

--- src/CheckAnswer/CheckAnswer.js
+++ src/CheckAnswer/CheckAnswer.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./CheckAnswer.css";
 
-export default function CheckAnswer() {
-  return (
-    <main className="check-anwser-container">
-      <p className="correct-answer-count">You scored 3/5 correct answers</p>
-      <button className="check-answer-button">Check Answer</button>
-    </main>
-  );
+export default function CheckAnswer(props) {
+  // run clicked() if 'check answer' button is clicked
+  // this will display the user's score and a button to start another quiz
+  function clicked() {
+    return (
+      <div className="check-anwser-container">
+        <p className="correct-answer-count">You scored 3/5 correct answers</p>
+        <button className="check-answer-button" onClick={props.handleClick}>
+          Play Again
+        </button>
+      </div>
+    );
+  }
+  // display 'check answer' button while waiting for user to finish quiz
+  function notClicked() {
+    return (
+      <div className="check-anwser-container">
+        <button className="check-answer-button" onClick={props.handleClick}>
+          Check Answer
+        </button>
+      </div>
+    );
+  }
+  const renderResults = props.buttonClicked ? clicked() : notClicked();
+  return <main className="main-container">{renderResults}</main>;
 }
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Quiz.css";
 import Question from "../Question/Question";
 import CheckAnswer from "../CheckAnswer/CheckAnswer";
 
 export default function Quiz(props) {
+  const [buttonClicked, setButtonClicked] = useState(false);
+  function handleClick() {
+    setButtonClicked((prevState) => !prevState);
+  }
   return (
     <main className="questions-container">
       <Question />
@@ -11,7 +15,7 @@ export default function Quiz(props) {
       <Question />
       <Question />
       <Question />
-      <CheckAnswer />
+      <CheckAnswer buttonClicked={buttonClicked} handleClick={handleClick} />
     </main>
   );
 }
